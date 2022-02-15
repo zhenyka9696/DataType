8613dda8abf95c42e979accb34bb7d9a10375a8f
alinapodrez <alinapodrez@gmail.com>
2022-02-15 21:10:15 +0300

    DataTypes

---
+++ src/HowCalculates.java
@@ -0,0 +1,22 @@
+public class HowCalculates {
+    public static void main(String[] args) {
+        int n=5/3;
+        System.out.println(n);
+        double m=5/3;
+        System.out.println(m);
+        double s=5.0/3.0;
+        System.out.println(s);
+        double q=5%3;
+        System.out.println(q);
+        double w=5.0/0;
+        System.out.println(w);
+        m=Math.sin(Math.PI/6);
+        System.out.println(m);
+        //n=5/0;
+        float n2=6.0f;
+        System.out.println(n2);
+
+        long k=24444444443l;
+    }
+
+}
